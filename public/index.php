ae00f30cc3d5de52e6f7c8e94bc5cc2feac59644
ehamrin <eh222ve@student.lnu.se>
2015-11-01 17:52:12 +0100

    Error reporting

--- public/index.php
+++ public/index.php
@@ -1,15 +1,19 @@
 <?php
 
 define('APP_ROOT', dirname(getcwd()) . DIRECTORY_SEPARATOR);
+
 define('DEBUG', TRUE);
+
+ini_set('display_errors', DEBUG);
+error_reporting(E_ALL);
+
 require_once APP_ROOT . 'includes/functions.php';
 session_start();
+
+$parameters = isset($_GET['url']) ? $_GET['url'] : '';
 
 if(!class_exists("Settings")){
     require_once APP_ROOT . 'includes/setup.php';
 }else{
-    new \Application($_GET['url']);
+    new \Application($parameters);
 }
-
-
-
