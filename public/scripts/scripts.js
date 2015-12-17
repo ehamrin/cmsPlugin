b7119232c283a8514e3bd9ccc3c86bb0153a9bf0
ehamrin <eh222ve@student.lnu.se>
2015-12-17 07:30:42 +0100

    Added offline cache

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -282,6 +282,20 @@
 
 $(document).ready(function(){
 
+    //make sure that Service Workers are supported.
+    if (navigator.serviceWorker) {
+        navigator.serviceWorker.register('service-worker.js', {scope: './'})
+            .then(function (registration) {
+                console.log(registration);
+            })
+            .catch(function (e) {
+                console.error(e);
+            })
+    } else {
+        console.log('Service Worker is not supported in this browser.');
+    }
+
+
     $('.slider').textSlider({
         auto_next: true,
         show_nav: false,
