c44c2dd0f72faafa83759b4cc093ccb6e63e4a6c
ehamrin <eh222ve@student.lnu.se>
2016-01-19 17:20:22 +0100

    Minor fixes

--- src/plugin/CookieBanner/CookieBanner.php
+++ src/plugin/CookieBanner/CookieBanner.php
@@ -29,8 +29,8 @@ class CookieBanner extends AbstractPlugin
             <div id="cookie_banner">
                 <div class="wrapper">
                 <p class="disclaimer">
-                    Vi använder cookies för att din upplevelse av webbplatsen ska bli bättre.
-                    Genom att fortsätta använda vår webbplats accepterar du våra cookies.
+                    Denna webbplats använder cookies för att din upplevelse av ska bli bättre.
+                    Genom att fortsätta använda webbplatsen accepterar du användningen av cookies.
                     <a target="_blank" href="http://sv.wikipedia.org/wiki/Webbkaka">Läs mer om cookies.</a>
 
                 </p>
