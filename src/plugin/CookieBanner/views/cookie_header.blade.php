b4514f6d20afcf5c0902076dc525fd24c9974379
ehamrin <eh222ve@student.lnu.se>
2016-01-30 12:41:53 +0100

    Refactored CookieBanner

---
+++ src/plugin/CookieBanner/views/cookie_header.blade.php
@@ -0,0 +1,12 @@
+<div id="cookie_banner">
+    <div class="wrapper">
+        <p class="disclaimer">
+            Denna webbplats använder cookies för att din upplevelse av ska bli bättre.
+            Genom att fortsätta använda webbplatsen accepterar du användningen av cookies.
+            <a target="_blank" href="http://sv.wikipedia.org/wiki/Webbkaka">Läs mer om cookies.</a>
+
+        </p>
+
+        <a href="?cookie_approval" class="button">Jag förstår</a>
+    </div>
+</div>
